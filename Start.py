ae4e6400e44c2ab644ff581cb1f34ca77749b72f
selinakaech <kaechsel@students.zhaw.ch>
2025-02-20 14:30:21 +0100

    Update Start.py

--- Start.py
+++ Start.py
@@ -3,4 +3,5 @@ import pandas as pd
 
 st.title("Meine erste Streamlit App")
 
-st.write("Diese App wurde von Selina Käch entwickelt.")
+st.write("Diese App wurde von Selina Käch entwickelt.
+          E-Mail Adresse: kaechsel@students.zhaw.ch")
