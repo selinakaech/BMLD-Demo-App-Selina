2cd9f6a25ed545af62b628a5a77dcae6e95a02da
selinakaech <kaechsel@students.zhaw.ch>
2025-02-25 18:02:37 +0100

    Verdünnungsrechner eingeführt

--- pages/1_Unterseite A.py
+++ pages/1_Unterseite A.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("Unterseite A")
+st.title("Verdünnungsrechner")
 
-st.write("Diese Seite ist eine Unterseite der Startseite.")
+st.header("Verdünnungsrechner")
+
+# Eingabe der Konzentrationen und Volumina
+c1 = st.number_input("Konzentration der Stammlösung (c1) in mol/L", min_value=0.0, step=0.01)
+v1 = st.number_input("Volumen der Stammlösung (v1) in L", min_value=0.0, step=0.01)
+c2 = st.number_input("Gewünschte Endkonzentration (c2) in mol/L", min_value=0.0, step=0.01)
+
+# Berechnung des Endvolumens
+if c1 > 0 and c2 > 0:
+    v2 = (c1 * v1) / c2
+    st.write(f"Das benötigte Endvolumen (v2) ist {v2:.2f} L")
+else:
+    st.write("Bitte geben Sie gültige Werte für c1 und c2 ein.")
 
