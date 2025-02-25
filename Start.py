a1e5e4e906ef3fe27de8cd425ab5675dba33d81c
selinakaech <kaechsel@students.zhaw.ch>
2025-02-25 18:40:24 +0100

    Start angepasst

--- Start.py
+++ Start.py
@@ -6,7 +6,13 @@ st.title("Meine erste Streamlit App")
 st.write("Diese App wurde von Selina Käch entwickelt.")
 st.write("E-Mail Adresse: kaechsel@students.zhaw.ch")
 
-# Link zur Molmassenrechner-Seite
-if st.button("Gehe zu Molmassenrechner"):
-    st.experimental_rerun()
-    st.write("pages/Molmassenrechner.py")
+# Sidebar navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Gehe zu", ["Startseite", "Molmassenrechner", "Verdünnungsrechner"])
+
+if page == "Startseite":
+    st.write("Willkommen auf der Startseite!")
+elif page == "Molmassenrechner":
+    import pages.Molmassenrechner
+elif page == "Verdünnungsrechner":
+    import pages.Verdünnungsrechner
