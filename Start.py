513e6ababe497355d864c219ee434ec1253f6ba0
selinakaech <kaechsel@students.zhaw.ch>
2025-02-25 18:37:57 +0100

    Link zur Molmassen seite

--- Start.py
+++ Start.py
@@ -5,3 +5,8 @@ st.title("Meine erste Streamlit App")
 
 st.write("Diese App wurde von Selina Käch entwickelt.")
 st.write("E-Mail Adresse: kaechsel@students.zhaw.ch")
+
+# Link zur Molmassenrechner-Seite
+if st.button("Gehe zu Molmassenrechner"):
+    st.experimental_rerun()
+    st.write("pages/Molmassenrechner.py")
