2c3fa68811f9f866310388aec77d89a8f5e555ea
selinakaech <kaechsel@students.zhaw.ch>
2025-02-26 08:31:57 +0100

    Start.py angepasst

--- Start.py
+++ Start.py
@@ -5,16 +5,3 @@ st.title("Meine erste Streamlit App")
 
 st.write("Diese App wurde von Selina Käch entwickelt.")
 st.write("E-Mail Adresse: kaechsel@students.zhaw.ch")
-
-# Sidebar navigation
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Gehe zu", ["Startseite", "Molmassenrechner", "Verdünnungsrechner", "Multiplikationsrechner"])
-
-if page == "Startseite":
-    st.write("Willkommen auf der Startseite!")
-elif page == "Verdünnungsrechner":
-    import pages.Verdünnungsrechner
-    pages.Verdünnungsrechner.main()
-elif page == "Multiplikationsrechner":
-    import pages.Multiplikationsrechner
-    pages.Multiplikationsrechner.main()
