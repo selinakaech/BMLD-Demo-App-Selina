600466036e71ee0a6040c1f89e3fa8101d4e6de7
selinakaech <kaechsel@students.zhaw.ch>
2025-03-13 17:45:41 +0100

    update

--- pages/2_Molmassendaten.py
+++ pages/2_Molmassendaten.py
@@ -1,19 +1,13 @@
 import streamlit as st
 import pandas as pd
+from utils.data_manager import DataManager
 
-class DataManager:
-    def __init__(self):
-        if 'data_df' not in st.session_state:
-            st.session_state['data_df'] = pd.DataFrame()
+st.title('Molmassendaten')
 
-    def append_record(self, session_state_key, record_dict):
-        if session_state_key in st.session_state:
-            st.session_state[session_state_key] = st.session_state[session_state_key].append(record_dict, ignore_index=True)
-        else:
-            st.session_state[session_state_key] = pd.DataFrame([record_dict])
+# Load data from session state
+data_df = DataManager().get_data(session_state_key='data_df')
 
-    def get_data(self, session_state_key):
-        if session_state_key in st.session_state:
-            return st.session_state[session_state_key]
-        else:
-            return None
+if data_df is not None and not data_df.empty:
+    st.write(data_df)
+else:
+    st.write("Keine Daten verfügbar.")
