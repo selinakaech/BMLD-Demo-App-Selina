bab3a235aeae92ce7a85f8d2883554ef4d478138
selinakaech <kaechsel@students.zhaw.ch>
2025-03-13 17:13:26 +0100

    update

--- Start.py
+++ Start.py
@@ -2,17 +2,6 @@ import streamlit as st
 import pandas as pd
 from utils.data_manager import DataManager
 
-# initialize the data manager
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_DB")  # switch drive 
-
-# load the data from the persistent storage into the session state
-data_manager.load_user_data(
-    session_state_key='data_df', 
-    file_name='data.csv', 
-    initial_value = pd.DataFrame(), 
-    parse_dates = ['timestamp']
-    )
-
 st.title("ChemCalc-App")
 st.write("Diese Streamlit App ermöglicht die präzise Berechnung der Molmasse chemischer Verbindungen. Benutzer können die chemische Formel einer Verbindung eingeben, und die App berechnet automatisch die Molmasse basierend auf den Atommassen der enthaltenen Elemente.")
 
