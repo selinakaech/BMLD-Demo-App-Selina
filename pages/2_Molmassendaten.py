43ddd95d80ec56fa69e8c6c220d48a2d3226ac9f
selinakaech <kaechsel@students.zhaw.ch>
2025-03-13 17:21:55 +0100

    update

---
+++ pages/2_Molmassendaten.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+from utils.data_manager import DataManager
+
+st.title('Molmassendaten')
+
+# Load data from session state
+data_df = DataManager().get_data(session_state_key='data_df')
+
+if data_df is not None:
+    st.write(data_df)
+else:
+    st.write("Keine Daten verfügbar.")
