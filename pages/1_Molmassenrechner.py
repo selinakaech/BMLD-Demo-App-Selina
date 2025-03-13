9c47f7cc930d68c49e9771247ec0bf72ebf8ed44
selinakaech <kaechsel@students.zhaw.ch>
2025-03-13 17:09:21 +0100

    update

--- pages/1_Molmassenrechner.py
+++ pages/1_Molmassenrechner.py
@@ -1,22 +1,8 @@
 import streamlit as st
-import re
 import pandas as pd
 import altair as alt
-
-# Dictionary of elements and their molar masses
-elements = {
-    'H': 1.008, 'He': 4.0026, 'Li': 6.94, 'Be': 9.0122, 'B': 10.81, 'C': 12.011, 'N': 14.007, 'O': 15.999, 'F': 18.998, 'Ne': 20.180,
-    'Na': 22.990, 'Mg': 24.305, 'Al': 26.982, 'Si': 28.085, 'P': 30.974, 'S': 32.06, 'Cl': 35.45, 'Ar': 39.948, 'K': 39.098, 'Ca': 40.078,
-    'Sc': 44.956, 'Ti': 47.867, 'V': 50.942, 'Cr': 51.996, 'Mn': 54.938, 'Fe': 55.845, 'Co': 58.933, 'Ni': 58.693, 'Cu': 63.546, 'Zn': 65.38,
-    'Ga': 69.723, 'Ge': 72.63, 'As': 74.922, 'Se': 78.971, 'Br': 79.904, 'Kr': 83.798, 'Rb': 85.468, 'Sr': 87.62, 'Y': 88.906, 'Zr': 91.224,
-    'Nb': 92.906, 'Mo': 95.95, 'Tc': 98, 'Ru': 101.07, 'Rh': 102.91, 'Pd': 106.42, 'Ag': 107.87, 'Cd': 112.41, 'In': 114.82, 'Sn': 118.71,
-    'Sb': 121.76, 'Te': 127.60, 'I': 126.90, 'Xe': 131.29, 'Cs': 132.91, 'Ba': 137.33, 'La': 138.91, 'Ce': 140.12, 'Pr': 140.91, 'Nd': 144.24,
-    'Pm': 145, 'Sm': 150.36, 'Eu': 151.96, 'Gd': 157.25, 'Tb': 158.93, 'Dy': 162.50, 'Ho': 164.93, 'Er': 167.26, 'Tm': 168.93, 'Yb': 173.05,
-    'Lu': 174.97, 'Hf': 178.49, 'Ta': 180.95, 'W': 183.84, 'Re': 186.21, 'Os': 190.23, 'Ir': 192.22, 'Pt': 195.08, 'Au': 196.97, 'Hg': 200.59,
-    'Tl': 204.38, 'Pb': 207.2, 'Bi': 208.98, 'Th': 232.04, 'Pa': 231.04, 'U': 238.03, 'Np': 237, 'Pu': 244, 'Am': 243, 'Cm': 247, 'Bk': 247,
-    'Cf': 251, 'Es': 252, 'Fm': 257, 'Md': 258, 'No': 259, 'Lr': 262, 'Rf': 267, 'Db': 270, 'Sg': 271, 'Bh': 270, 'Hs': 277, 'Mt': 276,
-    'Ds': 281, 'Rg': 282, 'Cn': 285, 'Nh': 286, 'Fl': 289, 'Mc': 290, 'Lv': 293, 'Ts': 294, 'Og': 294
-}
+from functions.Molmassen-Calculator import create_result_dict
+from utils.data_manager import DataManager
 
 st.title('Molmassenrechner')
 
@@ -27,34 +13,12 @@ with st.form(key='element_form'):
 
 if submit_button:
     if compound:
-        def parse_compound(compound):
-            pattern = r'([A-Z][a-z]*)(\d*)'
-            matches = re.findall(pattern, compound)
-            parsed = []
-            for (element, count) in matches:
-                count = int(count) if count else 1
-                parsed.append((element, count))
-            return parsed
-
-        def calculate_molar_mass(compound, multiplier):
-            parsed_compound = parse_compound(compound)
-            total_mass = 0
-            element_masses = []
-            for element, count in parsed_compound:
-                if element in elements:
-                    element_mass = elements[element] * count * multiplier
-                    total_mass += element_mass
-                    element_masses.append((element, element_mass))
-                else:
-                    return None, None
-            return total_mass, element_masses
-
-        molar_mass, element_masses = calculate_molar_mass(compound, multiplier)
-        if molar_mass is not None:
-            st.write(f'Die Molmasse der Verbindung {compound} multipliziert mit {multiplier} ist {molar_mass} g/mol.')
+        result = create_result_dict(compound, multiplier)
+        if 'error' not in result:
+            st.write(f'Die Molmasse der Verbindung {compound} multipliziert mit {multiplier} ist {result["molar_mass"]} g/mol.')
 
             # Create a DataFrame for the bar chart
-            df = pd.DataFrame(element_masses, columns=['Element', 'Masse'])
+            df = pd.DataFrame(result['element_masses'], columns=['Element', 'Masse'])
             chart = alt.Chart(df).mark_bar().encode(
                 x='Element',
                 y='Masse'
@@ -63,8 +27,11 @@ if submit_button:
             )
             st.altair_chart(chart, use_container_width=True)
         else:
-            st.write('Ungültige chemische Verbindung. Bitte geben Sie eine gültige Verbindung ein.')
+            st.write(result['error'])
 
 # Add a reset button to clear the input fields
 if st.button('Zurücksetzen'):
     st.experimental_rerun()
+
+# update data in session state and save to persistent storage
+DataManager().append_record(session_state_key='data_df', record_dict=result)  
