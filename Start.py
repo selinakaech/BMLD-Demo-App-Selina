4568a3e728d2c34d32aae3c7f64713d34207271b
selinakaech <kaechsel@students.zhaw.ch>
2025-03-13 14:37:56 +0100

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import pandas as pd
-
 from utils.data_manager import DataManager
 
 # initialize the data manager
 data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_DB")  # switch drive 
 
 # load the data from the persistent storage into the session state
-data_manager.load_app_data(
+data_manager.load_user_data(
     session_state_key='data_df', 
     file_name='data.csv', 
     initial_value = pd.DataFrame(), 
