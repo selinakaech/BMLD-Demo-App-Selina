ad3f7da7a07585c8fade35cd82566447351f06a3
selinakaech <kaechsel@students.zhaw.ch>
2025-03-15 09:58:28 +0100

    update

--- pages/1_Molmassenrechner.py
+++ pages/1_Molmassenrechner.py
@@ -57,3 +57,6 @@ if submit_button:
 # Add a reset button to clear the input fields
 if st.button('Zurücksetzen'):
     st.experimental_rerun()
+
+    from utils.data_manager import DataManager
+    DataManager().append_record(session_state_key='data_df', record_dict=result)
